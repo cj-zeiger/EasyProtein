adfe94725597d71ca5f6fdd8e9c9f28af93f02f8
cj-zeiger <cjzeiger@gmail.com>
2015-03-18 20:38:22 -0400

    created new activity

--- app/src/main/java/com/zygr/easyprotein/app/ModifyFoodEntry.java
+++
@@ -1,9 +0,0 @@
-package com.zygr.easyprotein.app;
-
-
-public interface ModifyFoodEntry {
-    public void addFoodEntry(int calorie, int protein);
-    public void resetFoodEntry();
-    public void confirmReset();
-    public void dataModified();
-}
